3a2ad7b7d3a1cf2c0e97bb59aebe7589c5d268c8
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-16 12:05:52 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from './utils/Themes.js'
+import './index.css'
 import Home from './pages/Home';
 import NavBar from './components/Navbar';
+import styled from 'styled-components';
 
+const Portfolio = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+`;
 function App() {
+
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <div className="App">
+    <Portfolio>
+      <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
-    <NavBar/>
+        <NavBar />
         <Routes>
-          <Route path='/' exact element = {<Home/>}/>
+          <Route path='/' exact element={<Home />} />
         </Routes>
-    </BrowserRouter>
-    </div>
+      </BrowserRouter>
+    </ThemeProvider>
+    </Portfolio >
   );
 }
 
