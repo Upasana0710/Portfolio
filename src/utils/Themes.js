46b23a5002a04ea81d37c4c828126ad026b03f95
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-16 12:05:14 +0530

    added the dark theme

---
+++ src/utils/Themes.js
@@ -0,0 +1,9 @@
+export const darkTheme = {
+    bg:"#0a0a0a",
+    bgLight: "#1C1E27",
+    primary:"#7d3a70",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#121212",
+    button:"#5c5b5b",
+}
