d28a9ac097583d97a598623176c7ae7ddeb931c7
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-18 09:06:20 +0530

    formatting

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
+
 const AboutContainer = styled.div`
 background: ${({ theme }) => theme.bg};
 width: 100%;
