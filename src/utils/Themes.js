bda5d463bc0a69a95b52f650048a80d1f720aa0c
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-18 22:31:55 +0530

    changed the primary color

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,7 +1,7 @@
 export const darkTheme = {
     bg:"#0a0a0a",
     bgLight: "#1C1E27",
-    primary:"#7d3a70",
+    primary:"#2778cf",
     text_primary:"#F2F3F4",
     text_secondary:"#b1b2b3",
     card:"#121212",
