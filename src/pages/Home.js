5b3397d5518bff09e13e62e0b07d137042b634e6
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-05-27 01:22:36 +0530

    kept all pages in one

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,54 +1,78 @@
-import React from 'react'
+import React,{useState, useRef} from 'react'
+import { Link, Element } from 'react-scroll';
 import styled from 'styled-components';
-import Flower from '../images/flower.png';
 import '../index.css';
+import About from '../components/About.js';
+import Projects from '../components/Projects.js';
+import Skills from '../components/Skills.js';
+import Education from '../components/Education.js';
+import Welcome from '../components/Welcome'
+import { NavLink } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
-const HomeContainer = styled.div`
+const Container = styled.div`
 background: ${({ theme }) => theme.bg};
-width: 100%;
-height: 120vh;
 display: flex;
+flex-direction: column;
 justify-content: flex-start;
 align-items: flex-start;
+overflow-y: auto;
 `
-const Content = styled.div`
-width: 100%;
-position: relative;
-display: flex;
-justify-content: center;
-align-items: center;
-`
-const ImageContainer = styled.img`
-width: 500px;
-@media (max-width: 768px){
-    margin: 60px;
-    width: 1000px;
+const NavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  height: 70px;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0px 70px;
+  gap: 50px;
+  font-size: 18px;
+  @media (max-width: 768px){
+    display: block;
+    flex-direction: column;
+    width: 100px;
+    padding: 10px 10px;
+    z-index: 6;
   }
 `
-const Intro = styled.div`
-position: absolute;
-text-align: center;
-white-space: pre-line;
-top: 46%;
-left: 24%;
-font-size: 50px;
-color: ${({ theme }) => theme.text_primary};
-@media (max-width: 768px){
-    top: 26%;
-    left: 4%;
+const Nav = styled.div`
+padding: 8px 20px;
+border-radius: 4px;
+textDecoration: none;
+  &:hover{
+    background-color: ${({ theme }) => theme.primary};
   }
+`
+const Button = styled.div`
+cursor: pointer;
 `
 const Home = () => {
+  const [showNav, setShowNav] = useState(true);
+
   return (
-    <HomeContainer>
-        <Content>
-            <ImageContainer src={Flower}/>
-            <Intro>
-                HELLO, I'M UPASANA CHAUDHURI,<br/>
-                A WEB DEVELOPER
-            </Intro>
-        </Content>
-    </HomeContainer>
+    <Container>
+      <NavContainer>
+      <Button onClick={() => setShowNav(!showNav)}><MenuIcon sx={{ color: "white" }} /></Button>
+      {showNav ?
+        <>
+          <NavLink to='/' style={{ color: 'inherit', textDecoration: 'none' }}><Nav>Home</Nav></NavLink>
+          <Link to="target" smooth={true} duration={500} style={{ color: 'inherit', textDecoration: 'none' }}><Nav>About</Nav></Link>
+          <a href="#skills" style={{ color: 'inherit', textDecoration: 'none' }}><Nav>Skills</Nav></a>
+          <a href="#projects" style={{ color: 'inherit', textDecoration: 'none' }}><Nav>Projects</Nav></a>
+          <a href="#education" style={{ color: 'inherit', textDecoration: 'none' }}><Nav>Education</Nav></a>
+        </>
+        :
+        <></>
+      }
+    </NavContainer>
+    <Welcome/>
+    <About id="about" />
+    <Skills id="skills"/>
+    <Projects id="projects"/>
+    <Education id="education" />
+    </Container>
   )
 }
 
